1132129b6852e9714a655edee3e7997525e7aab1
als8921 <als8921@naver.com>
2024-09-29 00:59:11 +0900

    refactor : 명령 함수 정리 및 미션 수행

--- src/main.py
+++ src/main.py
@@ -18,9 +18,6 @@ class Boat:
         self.position = [0, 0]
         self.psi = 0
         self.scan = [0] * 360
-        self.waypoints = [
-            [1.6417313533020206, 5.421214632689953]
-        ]
 
 class CallBack:
     threshold = 100.0  # 특정 거리 임계값 설정 (예: 100.0 센티미터)
@@ -37,13 +34,6 @@ class CallBack:
         distances[distances > CallBack.threshold] = 0  # 임계값 초과 시 0으로 변경
         boat.scan = distances
 
-    @staticmethod
-    def waypoint_callback(data):
-        if(data.point.z == -1):
-            boat.waypoints = []
-        else:
-            boat.waypoints.append([data.point.x, data.point.y])
-        print(boat.waypoints)
     @staticmethod
     def gps_callback(data):
         boat.position = data.data
@@ -72,20 +62,11 @@ def ros_init():
     gps_sub = message_filters.Subscriber("KABOAT/UTM", Float64MultiArray)
     imu_sub = message_filters.Subscriber("KABOAT/Heading", Float32)
 
-    rospy.Subscriber("/Waypoint", PointStamped, CallBack.waypoint_callback)
-
     ts = message_filters.ApproximateTimeSynchronizer([gps_sub, imu_sub], queue_size=10, slop=0.1, allow_headerless=True)
     ts.registerCallback(lambda gps_data, imu_data: (CallBack.gps_callback(gps_data), CallBack.imu_callback(imu_data)))
 
-
-
-
-
-def utm(data):
-    utm_x, utm_y, _, _ = utm.from_latlon(data.latitude, data.longitude)
-
-def Automode(goal_x,goal_y):
-    rate = rospy.Rate(10)  # 10Hz로 퍼블리시
+def Autonomous(goal_x,goal_y):
+    rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         loginfoOnce(f"[Autonomous Mode] [{goal_x:.2f}, {goal_y:.2f}] Start")
         
@@ -102,83 +83,52 @@ def Automode(goal_x,goal_y):
         rate.sleep()
 
 
-def rotate_mode(goal_psi):
+def Rotate(goal_psi):
+    rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         path = AutonomousModule.rotate(boat, goal_psi)
 
         command_publish.publish(Float32MultiArray(data=path))
-        rospy.loginfo(path)
 
-        loginfoOnce(f"[Autonomous Mode] WayPoint Empty")
         if abs(path[0])<1:
             print(boat.psi ,path[0])
-            loginfoOnce(f"[Rotate Finish]")
             break
+        
+        rate.sleep()
     
 
 def Wait(wait_time):
-    first_time=time.time()
-    while(time.time() - first_time < wait_time):
+    rate = rospy.Rate(10)
+    for _ in range(10 * wait_time):
         command_publish.publish(Float32MultiArray(data=[0,0]))
+        rate.sleep()
 
 
+def Tuning(goal_psi):
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        path = AutonomousModule.rotate(boat, goal_psi)
+
+        command_publish.publish(Float32MultiArray(data=path))
+        
+        rate.sleep()
 
 
 
 
 def main():
     ros_init()
-    rate = rospy.Rate(10)  # 10Hz로 퍼블리시
-    # Automode(-3,3)
-    # Automode(-25,4)
-    # Automode(-3,3)
-    # rotate_mode(0)
-    # command_publish.publish(Float32MultiArray(data=[0,0]))
-    # time.sleep(2)
-    # # Wait(3)
-    # Automode(64,25)
-    # rotate_mode(-90)
-    # # Wait(3)
-    # command_publish.publish(Float32MultiArray(data=[0,0]))
-    # time.sleep(2)
-    # Automode(0,25)
-    # rotate_mode(-180)
-    # # Wait(3)
-    # command_publish.publish(Float32MultiArray(data=[0,0]))
-    # time.sleep(2)
-    # Automode(0,0)
-    # print('Rcsdjhfewiunf')
+    # Autonomous(-3,3)
+    # Rotate(-90)
+    # Wait(3)
+    # Autonomous(-25,4)
+    # Rotate(90)
+    # Wait(3)
+    # Autonomous(-3,3)
+    print("MISSION CLEAR")
 
 
 
-
-
-
-
-    
-    # rotate_mode(180)
-    # Automode(0,0)
-    # rotate_mode(180)
-    # Automode(5,5)
-
-    """
-    배를 제자리에서 회전하도록 만드는 부분
-    """
-    #############################################################################################################################################################
-    
-    while not rospy.is_shutdown():
-            
-        """
-        배를 제자리에서 회전하도록 만드는 부분
-        """
-        path = AutonomousModule.rotate(boat, 0)
-
-        command_publish.publish(Float32MultiArray(data=path))
-        rospy.loginfo(path)
-        print(boat.psi)
-
-        rate.sleep()  # 주기 유지
-    ##########################################
 
 
 
