85a0e79fab504238327ecf923fcff39540956f8f
als8921 <als8921@naver.com>
2024-09-28 23:00:17 +0900

    [REFACTOR] PWM Publish 코드 정리

--- src/Control/PWMPublish.py
+++ src/Control/PWMPublish.py
@@ -11,9 +11,7 @@ import SETTINGS
 class MotorController:
     def __init__(self):
         rospy.init_node('motor_controller', anonymous=True)
-        self.control_pub = rospy.Publisher('/control', Int16MultiArray, queue_size=1)
-        self.i = 0.0
-        # command 배열을 구독
+        self.control_pub = rospy.Publisher('/control', Int16MultiArray, queue_size = 10)
         rospy.Subscriber('/command', Float32MultiArray, self.command_callback)
 
         self.last_error = 0.0
@@ -21,10 +19,7 @@ class MotorController:
         self.last_time = time.time()
 
     def command_callback(self, data):
-        if len(data.data) < 2:
-            rospy.logwarn("command 배열의 길이가 충분하지 않습니다.")
-            return
-        
+
         psi_error = data.data[0]
         tauX = data.data[1]
         tauN = self.pd_control(psi_error)
@@ -42,27 +37,21 @@ class MotorController:
             control_values.data = [0, 0, 0]
             
         self.control_pub.publish(control_values)
-        # print(control_values)
 
     def pd_control(self, error):
-        # 현재 시간
         current_time = time.time()
-        dt = current_time - self.last_time  # 시간 간격 계산
-        self.last_time = current_time  # 현재 시간을 마지막 시간으로 업데이트
+        dt = current_time - self.last_time
+        self.last_time = current_time
         
-        # PD 제어 계산
         derivative = (error - self.last_error) / dt if dt > 0 else 0
         self.last_error = error
         
-        tauN = SETTINGS.Kp * error + SETTINGS.Kd * derivative
-        return tauN
-
-    def run(self):
-        rospy.spin()
+        output = SETTINGS.Kp * error + SETTINGS.Kd * derivative
+        return output
 
 if __name__ == '__main__':
     try:
         controller = MotorController()
-        controller.run()
+        rospy.spin()
     except rospy.ROSInterruptException:
         pass
